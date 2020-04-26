b83841ef319557bc1298af3f46e036db5c919eb1
OlgaSergienkoS <63264244+OlgaSergienkoS@users.noreply.github.com>
2020-04-26 21:59:40 +0300

    Update square.py

--- square.py
+++ square.py
@@ -3,7 +3,7 @@ from myxml import downloadSquareFromXML, saveSquareInXml
 
 class MagickSquare:
 
-    def __init__(self, N = 1): #значение по умолчанию 
+    def __init__(self, N = 1): 
         self.N = N
         self.square = np.zeros((N,N), dtype=int)
 
@@ -14,15 +14,15 @@ class MagickSquare:
         saveSquareInXml(self.N, self.square)
 
     def ismagick(self):
-        if np.amin(self.square) == 0:#проверяет на то, что если есть какой-то 0, то точно не магический 
+        if np.amin(self.square) == 0:
             return False
-        sq = np.copy(self.square)#создает копию массива
-        sq = np.reshape(sq, self.N**2)#превращает в строчку
-        sq.sort()#сортирует
-        for i in range(1, self.N**2):#смотрим есть ли повторяющиеся элементы, если есть то не магический 
+        sq = np.copy(self.square)
+        sq = np.reshape(sq, self.N**2)
+        sq.sort()
+        for i in range(1, self.N**2):
             if sq[i] == sq[i-1]:
                 return False
-        sum = self.N*(self.N**2 + 1)/2#формула магического квардрата
+        sum = self.N*(self.N**2 + 1)/2
         if (np.sum(self.square, axis=0) == sum).all() and (np.sum(self.square, axis=1) == sum).all() and sum == np.diag(self.square).sum() and np.diag(np.fliplr(self.square)).sum() == sum:
             return True
         return False
